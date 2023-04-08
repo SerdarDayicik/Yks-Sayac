b4e91e9c29ed8ce6d9f92a364b3c1d82ff4f50f1
Serdar Dayicik <103996688+SerdarDayicik@users.noreply.github.com>
2023-04-09 00:03:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function updateCountdown() {
   const seconds2 = Math.floor((distance2 % (1000 * 60)) / 1000);
 
   // HTML elementlerinde güncelleme yap
-  countdownEl2.innerHTML = `${days2} gün, ${hours2} saat, ${minutes2} dakika, ${seconds2} saniye kaldı.`;
+  countdownEl2.innerHTML = `${days2} gün, ${hours2} saat, ${minutes2} dakika, ${seconds2} saniye`;
 }
 
 
